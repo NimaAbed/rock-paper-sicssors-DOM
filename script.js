b6815ba1a9e0c5ccc14b7c30b4f83fa0e7a86c27
Nima Abed <abodnima84@gmail.com>
2023-05-16 14:52:23 +0330

    Add DOM To Show Win Or Lose

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 const choises =["rock","paper","sicssor"]
 const btn = document.querySelectorAll("button")
-btn.forEach(item => {
+const ul = document.createElement("ul")
+const cont = document.querySelector(".container") 
+btn.forEach(function(item) {
     item.addEventListener("click",function(){
         const random =Math.round(Math.random()*2)
         const computerChoises =choises[random]
+        const li = document.createElement("li")
         if(item.id === computerChoises){
             console.log(`It's Tie || Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+            li.innerText =`It's Tie || Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
         }else if(item.id === "rock"){
             if(computerChoises === "sicssor"){
                 console.log(`You Win!||Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+                li.innerText =`You Win!|| Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
             }else{
                 console.log(`You Lose!||Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+                li.innerText=`You Lose!|| Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
             }
         }else if(item.id === "paper"){
             if(computerChoises === "rock"){
                 console.log(`You Win!||Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+                li.innerText =`You Win!|| Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
             }else{
                 console.log(`You Lose!||Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+                li.innerText=`You Lose!|| Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
             }
         }else if(item.id === "sicssor"){
             if(computerChoises === "paper"){
                 console.log(`You Win!||Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+                li.innerText=`You Win!|| Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
             }else{
                 console.log(`You Lose!||Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`)
+                li.innerText =`You Lose!|| Your Choise Is ${item.id} & Computer Choise Is ${computerChoises}`
             }
         }
+        ul.appendChild(li)
+        console.log(ul)
+        
     })
+    
 })
+cont.appendChild(ul)
